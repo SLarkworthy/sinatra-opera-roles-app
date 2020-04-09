25f2f4f477a50da30a4b6db1f67f639298702d03
SLarkworthy <Sarah.Larkworthy@gmail.com>
2020-04-09 18:48:30 -0400

    stubbed login and signup in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,17 @@
 class UsersController < ApplicationController
 
+    get '/signup' do
+    end
+
+    post '/signup' do
+    end
+    
     get '/login' do
         erb :'users/login'
     end
+
+    post '/login' do
+    end
+
 
 end
