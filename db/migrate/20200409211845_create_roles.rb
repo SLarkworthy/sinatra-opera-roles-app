8b44e55d3a508be94c1b8b188972438f9189f06b
SLarkworthy <Sarah.Larkworthy@gmail.com>
2020-04-09 17:35:24 -0400

    added roles table and associations

---
+++ db/migrate/20200409211845_create_roles.rb
@@ -0,0 +1,20 @@
+class CreateRoles < ActiveRecord::Migration
+  def change
+    create_table :roles do |t|
+      t.string :name
+      t.string :opera
+      t.string :composer
+      t.boolean :researched null: false, default: false
+      t.boolean :translated null: false, default: false
+      t.boolean :listened null: false, default: false
+      t.boolean :learned null: false, default: false
+      t.boolean :coached null: false, default: false
+      t.boolean :memorized null: false, default: false
+      t.text :private_notes
+      t.text :public_notes
+      t.integer :user_id
+
+      t.timestamps null: false
+    end
+  end
+end
