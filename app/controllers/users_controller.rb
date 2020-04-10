6c90bb4b152593e8af782a7312804d87e1d06279
SLarkworthy <Sarah.Larkworthy@gmail.com>
2020-04-10 11:51:22 -0400

    set up sessions, updated login post route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,18 @@ class UsersController < ApplicationController
     end
 
     post '/login' do
-        binding.pry
+        @user = User.find_by(email: params[:email])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect "/users/#{@user.id}" #to be changed
+        else
+            #flash message telling them their email/password were incorrect
+            redirect '/login'
+        end
+    end
+
+    get '/users/:id' do
+        "Soon to be the user show page"
     end
 
 
