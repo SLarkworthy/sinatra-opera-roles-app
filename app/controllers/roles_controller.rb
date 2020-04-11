85aa72e01a645a46632417d5c8e6c01ee095aa9e
SLarkworthy <Sarah.Larkworthy@gmail.com>
2020-04-11 14:02:29 -0400

    stubbed get and post routes for new roles

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,7 +1,11 @@
 class RolesController < ApplicationController
 
-    get '/roles/new'
-        
+    get '/roles/new' do
+        erb :'roles/new'
+    end
+
+    post '/roles' do
+        erb :'roles/show'
     end
 
 end
